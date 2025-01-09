8698a85c985b70af72e064870baf965adf6f49db
Silpakorn <87792806+Silpakorn-D@users.noreply.github.com>
2025-01-09 09:47:12 +0700

    Update settings.py
    Decorate as per Linter

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-                os.path.join(BASE_DIR,'frontend/static'),
+                os.path.join(BASE_DIR, 'frontend/static'),
                 os.path.join(BASE_DIR, 'frontend/build'),
                 os.path.join(BASE_DIR, 'frontend/build/static'),
                 ],
@@ -94,7 +94,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.\
+            UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -139,17 +140,19 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'frontend/static'),
+        os.path.join(BASE_DIR, 'frontend/static'),
         os.path.join(BASE_DIR, 'frontend/build'),
         os.path.join(BASE_DIR, 'frontend/build/static'),
         ]
 
 
-ALLOWED_HOSTS=[
+ALLOWED_HOSTS = [
         'localhost',
-        'https://artsilpakorn-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+        'https://artsilpakorn-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.\n
+                proxy.cognitiveclass.ai',
         ]
 CSRF_TRUSTED_ORIGINS=[
-        'https://artsilpakorn-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+        'https://artsilpakorn-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.\n
+                proxy.cognitiveclass.ai',
         ]
 
