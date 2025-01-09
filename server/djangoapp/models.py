34918d53ea1ec57dfd450f2c7b444832903cd946
Silpakorn <87792806+Silpakorn-D@users.noreply.github.com>
2025-01-09 10:32:57 +0700

    Update models.py
    linter

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -47,6 +47,5 @@ class CarModel(models.Model):
                 MinValueValidator(2015)
         ])
     # Other fields as needed
-    
     def __str__(self):
         return self.name  # Return the name as the string representation
