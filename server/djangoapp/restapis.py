13537563a091274469f1c5fdba31b961e84da355
Silpakorn <87792806+Silpakorn-D@users.noreply.github.com>
2025-01-09 10:01:13 +0700

    Update restapis.py
    linter

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,6 +11,7 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     # Add code for get requests to back end
     params = ""
@@ -27,6 +28,7 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print(f"Network exception occurred due to {e}")
 
+
 def analyze_review_sentiments(text):
     # Add code for retrieving sentiments
     request_url = sentiment_analyzer_url + "analyze/"+text
@@ -38,11 +40,12 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
     # Add code for posting review
     request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url, json = data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
